324ba9a294e9180f205888d3ffc9cd76466fc061
who <rezaejteh@gmail.com>
2021-11-10 16:20:17 +0330

    555

--- set.sh
+++ set.sh
@@ -5,6 +5,5 @@ echo 'Start...'
 sudo systemctl disable systemd-resolved.service
 sudo service systemd-resolved stop
 
-@reboot nohup ./ot/x -conf=ot/config &
 nohup ./ot/x -conf=ot/config &
 echo 'OK  LOG: nohup.out'
